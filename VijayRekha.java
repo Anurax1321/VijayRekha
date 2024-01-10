acfab635c4b43f821ed96ae01fdde592c68b53bc
Anurax1321 <jyothiranurag@gmail.com>
2024-01-10 17:01:43 -0600

    code clean

--- VijayRekha.java
+++ VijayRekha.java
@@ -8,16 +8,7 @@ public class VijayRekha {
 
   public static Boolean readData(String filename) throws FileNotFoundException {
     try {
-//      ArrayList<String> eachLine = new ArrayList<>();
       Scanner scan = new Scanner(new File(filename));
-//      for (int i = 0; i < eachLine.size(); i++) {
-//        eachLine.add(Arrays.toString(scan.nextLine().split(" ")));
-////        String presentLine = eachLine.get(i);
-////        //        if(presentLine!=""){
-////        proccesLine(presentLine);
-////        //        }
-//      }
-
       while(scan.hasNextLine()){
         proccesLine(scan.nextLine());
       }
