06b45b34889e9ffbab41e4f6faa56eaa0cf8015a
Anurax1321 <jyothiranurag@gmail.com>
2024-01-10 14:42:32 -0600

    still trying to get the first and last items of the line

--- VijayRekha.java
+++ VijayRekha.java
@@ -10,11 +10,24 @@ public class VijayRekha {
     try {
       Scanner scan = new Scanner(new File(filename));
       if (scan.hasNextLine()) {
+        ArrayList<String> eachLine = new ArrayList<>();
         ArrayList<String> split = new ArrayList<>();
-        while (scan.hasNext()) {
-          split.add(scan.next());
-        }
+//        while (scan.hasNext()) {
+//          split.add(scan.next());
+//        }
+        eachLine.add(Arrays.toString(scan.nextLine().split(" ")));
+
+//        eachLine.add(scan.nextLine());
+        split.add(eachLine.get(0));
+
+//        for(int i=0; i<eachLine.size();i++){
+//          System.out.println(eachLine.get(i));
+//        }
+        System.out.println(eachLine.size());
+
         System.out.println(split);
+
+//        split.add();
       }
 
     } catch (FileNotFoundException e) {
