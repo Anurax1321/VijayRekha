6bab3b79982dc4bb94dacbfc7b8190755830e6de
Anurax1321 <jyothiranurag@gmail.com>
2024-01-10 20:00:08 -0600

    trying to get into 2d Array

--- VijayRekha.java
+++ VijayRekha.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 public class VijayRekha {
   static ArrayList<String> head = new ArrayList<>();
 
-  public static Boolean readData(String filename) throws FileNotFoundException {
+  public static Boolean readData(String filename, int limit) throws FileNotFoundException {
     try {
       Scanner scan = new Scanner(new File(filename));
       head.add("Name");
+
       while (scan.hasNextLine()) {
-        proccesLine(scan.nextLine());
+        proccesLine(scan.nextLine(), limit);
       }
       System.out.println(head);
     } catch (FileNotFoundException e) {
@@ -20,7 +21,7 @@ public class VijayRekha {
     return true;
   }
 
-  public static void proccesLine(String line) {
+  public static void proccesLine(String line, int limit) {
     if (line != "") {
       String[] word = line.split("\t");
       if (line.startsWith("Compound")) {
@@ -29,7 +30,16 @@ public class VijayRekha {
       if (word.length > 1 && line.matches("^\\d.*")) {
 
         System.out.println(line);
-//        System.out.println(word.length);
+        //        System.out.println(word.length);
+        String[][] data = new String[limit][];
+//        data[0][] = head;
+        for (int i = 1; i <= limit; i++) {
+          if(line.startsWith((String) i)){
+
+          }
+        data[i][]= word[1] ;
+        }
+
       }
     }
   }
@@ -38,7 +48,8 @@ public class VijayRekha {
     System.out.println("Hello world!");
     String filepath = "C:\\Users\\jyoth\\IdeaProjects\\VijayRekha\\src\\08012024_AA.txt";
 
-    Boolean result = readData(filepath);
+    int limit = 22;
+    Boolean result = readData(filepath, limit);
 
     System.out.println(result);
 
