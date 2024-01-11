13f7309733980f09b21512b1a7ca7eca1328020c
Anurax1321 <jyothiranurag@gmail.com>
2024-01-10 23:41:59 -0600

    got responses; time to merge two arrays

--- VijayRekha.java
+++ VijayRekha.java
@@ -25,28 +25,46 @@ public class VijayRekha {
         }
       }
       ArrayList<String> patientNames = new ArrayList<>();
+//      String[][] Response = new String[limit + 1][head.size()];
+      ArrayList<String> responses = new ArrayList<>();
       for (int i = 1; i <= limit; i++) {
         String pattern = "^" + i + "\\D";
         for (int j = 0; j < data.size(); j++) {
-          if (data.get(j).matches(pattern+ ".*")) {
-//            System.out.println(data.get(j));
-            String[] dataLine= data.get(j).split("\t");
+          if (data.get(j).matches(pattern + ".*")) {
+            //            System.out.println(data.get(j));
+            String[] dataLine = data.get(j).split("\t");
 
-            if (!(patientNames.contains(dataLine[1]))){
+            if (!(patientNames.contains(dataLine[1]))) {
               patientNames.add(dataLine[1]);
             }
-//            System.out.println(dataLine[1]);
-//            for(String item : dataLine){
-//              System.out.println(item);
-//            }
+            //            try {
+
+
+
+            responses.add(dataLine[dataLine.length - 1]);
+
+
+
+
+
+            //            }catch (IndexOutOfBoundsException e){
+            //              System.out.println("N/A");
+            //            }
+            //            for(String item : dataLine){
+            //              System.out.println(item);
+            //            }
           }
         }
       }
-      System.out.println(patientNames);
-      System.out.println(patientNames.size());
+      //      System.out.println(patientNames);
+      //      System.out.println(patientNames.size());
+      System.out.println(responses);
+      System.out.println(responses.size());
 
       //      System.out.println(data);
       System.out.println(head);
+      System.out.println(head.size());
+
     } catch (FileNotFoundException e) {
       throw new FileNotFoundException();
     }
